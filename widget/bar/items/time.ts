9839f9b516b6b0a7ef1be5dbd07bad8f52aec05e
LilleAila <olai.solsvik@gmail.com>
2024-05-23 17:19:03 +0200

    added date tooltip

--- widget/bar/items/time.ts
+++ widget/bar/items/time.ts
@@ -1,6 +1,9 @@
 const date = Variable("", {
   poll: [1000, 'date +"%H\n%M"'],
 });
+const full_date = Variable("", {
+  poll: [1000, 'date +"%A %d. %B %Y, Uke %V"'],
+});
 
 export default () =>
   Widget.Label({
@@ -8,4 +11,7 @@ export default () =>
     label: date.bind(),
     //hpack: "center",
     hexpand: true,
+    tooltip_text: full_date
+      .bind()
+      .transform((text) => text.charAt(0).toUpperCase() + text.slice(1)),
   });
