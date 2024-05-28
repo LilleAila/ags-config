6564417bf3fa19b898dd952207dc2e51ab4bf39c
LilleAila <olai.solsvik@gmail.com>
2024-05-28 14:19:15 +0200

    put tray into revealer

--- widget/bar/items/tray.ts
+++ widget/bar/items/tray.ts
@@ -38,14 +38,44 @@ const SysTrayItem = (item) =>
       ),
   });
 
-export default () =>
-  Widget.Box({
-    class_name: "bar-item tray",
+export default () => {
+  const revealer = Widget.Revealer({
+    transition: "slide_down",
+    transitionDuration: 250,
+    reveal_child: false,
+    child: Widget.Box({
+      class_name: "tray",
+      vertical: true,
+      hexpand: true,
+      hpack: "center",
+    }).bind("children", systemtray, "items", (i) =>
+      i
+        // .filter(({ id }) => !ignore.value.includes(id))
+        .map(SysTrayItem),
+    ),
+  });
+
+  const revealer_icon = Variable("pan-down-symbolic");
+
+  const reveal_button = Widget.Button({
+    class_name: "show-tray-button",
+    tooltip_text: "Show system tray",
+    on_clicked: () => {
+      revealer.reveal_child = !revealer.reveal_child;
+      revealer_icon.value = revealer.reveal_child
+        ? "pan-up-symbolic"
+        : "pan-down-symbolic";
+    },
+    child: Widget.Icon({ icon: revealer_icon.bind(), size: 20 }),
+  });
+
+  return Widget.Box({
+    class_name: "bar-item tray-wrapper",
     vertical: true,
-    hexpand: true,
-    hpack: "center",
-  }).bind("children", systemtray, "items", (i) =>
-    i
-      // .filter(({ id }) => !ignore.value.includes(id))
-      .map(SysTrayItem),
-  );
+    children: [
+      reveal_button,
+      revealer,
+      //
+    ],
+  });
+};
