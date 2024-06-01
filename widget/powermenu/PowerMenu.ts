0a53004ec024cf28812b99ac4e9e462c751c0405
LilleAila <olai.solsvik@gmail.com>
2024-06-01 18:42:51 +0200

    added powermenu keybinds

--- widget/powermenu/PowerMenu.ts
+++ widget/powermenu/PowerMenu.ts
@@ -1,5 +1,39 @@
-import Menu from "./Menu.ts";
 import { RevealerWindow } from "lib/widgets";
+const Button = (icon: string, click: any, tooltip: string) =>
+  Widget.Button({
+    class_name: "powermenu-button",
+    child: Widget.Icon({ icon: icon, size: 48 }),
+    onClicked: click,
+    hexpand: false,
+    vexpand: false,
+    hpack: "center",
+    vpack: "center",
+    "tooltip-text": tooltip,
+  });
+
+const lock = () => Utils.exec("loginctl lock-session");
+const suspend = () => Utils.exec("systemctl suspend");
+const poweroff = () => Utils.exec("systemctl poweroff");
+const reboot = () => Utils.exec("systemctl reboot");
+const logout = () => Utils.exec("hyprctl dispatch exit");
+
+const Menu = () => {
+  const buttons = [
+    Button("lock-symbolic", lock, "Lock (l)"),
+    Button("system-suspend-symbolic", suspend, "Suspend (s)"),
+    Button("system-shutdown-symbolic", poweroff, "Power off (p)"),
+    Button("system-reboot-symbolic", reboot, "Reboot (r)"),
+    Button("application-exit-symbolic", logout, "Log out (q)"),
+  ];
+
+  return Widget.Box({
+    class_name: "powermenu-menu",
+    children: buttons,
+    vertical: true,
+    hexpand: false,
+    vexpand: false,
+  });
+};
 
 export default (monitor: number = 0) =>
   RevealerWindow(
@@ -9,6 +43,18 @@ export default (monitor: number = 0) =>
     Menu(),
     150,
     "slide_right",
-    { class_name: "powermenu", margins: [8, 8, 8, 8] },
+    {
+      class_name: "powermenu",
+      margins: [8, 8, 8, 8],
+      keymode: "exclusive",
+      setup: (self: any) => {
+        self.keybind("Escape", () => App.closeWindow(`powermenu${monitor}`));
+        self.keybind("l", lock);
+        self.keybind("s", suspend);
+        self.keybind("p", poweroff);
+        self.keybind("q", logout); // "quit"
+        self.keybind("r", reboot);
+      },
+    },
     { vertical: true, vpack: "start", hpack: "start" },
   );
