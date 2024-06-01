1178f8291d5f3c157632647d4d63f2e31a3a2691
LilleAila <olai.solsvik@gmail.com>
2024-06-01 18:26:06 +0200

    added togglePopup to globalThis

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import Bar from "widget/bar/Bar";
 import OSD from "widget/osd/OSD";
 import PowerMenu from "widget/powermenu/PowerMenu";
 import AppLauncher from "widget/applauncher/AppLauncher";
-import { forMonitors } from "lib/utils";
+import { forMonitors, togglePopup } from "lib/utils";
 
 App.config({
   style: App.configDir + "/style.css",
@@ -13,3 +13,6 @@ App.config({
     ...forMonitors(AppLauncher),
   ],
 });
+
+// Make the function accessible from ags -r to be used in hyprland
+globalThis.togglePopup = togglePopup;
